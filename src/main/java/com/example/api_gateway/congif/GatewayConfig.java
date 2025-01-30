bbcbd28d2ef4c0c6459b455589fe1f1176adba06
Bruno <brunomoron56@gmail.com>
2025-01-30 01:26:20 -0300

    enabling security

--- src/main/java/com/example/api_gateway/congif/GatewayConfig.java
+++ src/main/java/com/example/api_gateway/congif/GatewayConfig.java
@@ -1,5 +1,6 @@
 package com.example.api_gateway.congif;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,15 +9,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public RouteLocator routes(RouteLocatorBuilder routeBuilder){
 
         return routeBuilder.routes()
+                .route("userService", route -> route.path("/api/auth/**")
+                        .uri("lb://user-service"))
                 .route("userService", route -> route.path("/api/users/**")
-                .uri("lb://user-service"))
+                        .filters(filter -> filter.filter(jwtAuthenticationFilter))
+                        .uri("lb://user-service"))
                 .route("productService", route -> route.path("/api/products/**")
+                        .filters(filter -> filter.filter(jwtAuthenticationFilter))
                         .uri("lb://product-service"))
                 .route("orderService", route -> route.path("/api/orders/**")
+                        .filters(filter -> filter.filter(jwtAuthenticationFilter))
                         .uri("lb://order-service"))
                 .build();
 
